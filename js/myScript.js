d39bde94dbff63f78ca749b500598389a74f6068
Carlos <carlos_valencia@live.com>
2016-07-16 18:10:33 -0500

    Contributors Section Added

--- js/myScript.js
+++ js/myScript.js
@@ -4,10 +4,41 @@ $( document ).ready(function() {
     	console.log("i was clikced");
     	$('.about-wrapper').addClass('is-showing');
     	$('.about-extended').show();
-    })
+    });
 
     $("#back-about-button").click(function(event){
     	$('.about-wrapper').removeClass('is-showing');
     	$('.about-extended').hide(800);
-    })
+    });
+
+	mySlider = new Slider;
+	//setInterval(mySlider.goRight, 1000);
 });
+
+var Slider = function(){
+	var slides = $(".slider-wrapper").find(".slider-card").length - 1
+	var currentSlide = 0;
+	var slideWidth = $(".slider-card").width();
+	var myInterval;
+
+	$("#slider-left").click(function(event){
+		goLeft();
+	});
+
+	$("#slider-right").click(function(event){
+		goRight();
+	});
+
+	function goRight(){
+		currentSlide = currentSlide === slides ? 0 : currentSlide + 1;
+		$(".slider-wrapper").css({"margin-left" : -slideWidth * currentSlide});	
+	}
+
+	function goLeft(){
+		currentSlide = currentSlide === 0 ? slides : currentSlide - 1;
+		$(".slider-wrapper").css({"margin-left" : -slideWidth * currentSlide});
+	}
+
+	myInterval = setInterval(goRight, 3000);
+
+};
